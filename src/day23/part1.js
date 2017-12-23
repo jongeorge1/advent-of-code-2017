e4745776b2fb6df08cbe2ae11c44415632a848ff
Jonathan George <jon@jonathangeorge.co.uk>
2017-12-23 23:31:37 +0000

    Day 23 - now I understand what it's doing, part 1 is a trivial calculation.

--- src/day23/part1.js
+++ src/day23/part1.js
@@ -1,25 +1,6 @@
-let Processor = require('./processor');
-
 class Day23Part1 {
   process(input) {
-    let commands = input.split('\n').map(el => {
-      let vals = el.split(' ');
-      return {
-        full: el,
-        cmd: vals[0],
-        reg: vals[1],
-        val: vals[2]
-      };
-    });
-
-    let processor = new Processor();
-
-    while (processor.nextInstruction >= 0 && processor.nextInstruction < commands.length) {
-      let cmd = commands[processor.nextInstruction];
-      processor[cmd.cmd](cmd.reg, cmd.val);
-    }
-
-    return processor.muls;
+    return Math.pow(+input.split('\r\n')[0].split(' ')[2] - 2, 2);
   }
 }
 
